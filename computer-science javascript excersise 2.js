773c0180d973ebaecd24356743bf85c429e9ef13
18Dmartin <112864911+18Dmartin@users.noreply.github.com>
2022-09-29 11:56:57 +0100

    Add files via upload

--- computer-science javascript excersise 2.js
+++ computer-science javascript excersise 2.js
@@ -1,6 +1,66 @@
-let a = (prompt("enter number"));
-if((value%2) == 0){
-console.log("true");
+//Q1
+let a = Number(prompt("enter number"));
+if((a%2)==0){
+    console.log("even");
+}else if((a%2)!=0){
+    console.log("odd");
+}
+
+
+//Q2
+let b = Number(prompt("enter first number"));
+let c = Number(prompt("enter second number"));
+if (b> c){
+    console.log("the first number is larger");
+}else if(b< c){
+    console.log("the second number is bigger");
 }else{
-    console.log("false")
+    console.log("they are both equal");
+}
+
+//Q3
+let d = Number(prompt("enter 1/3 numbers"));
+let f = Number(prompt("enter 2/3 numbers"));
+let g = Number(prompt("enter 3/3 numbers"));
+if (d > f){
+    console.log("the first Number is largest")
+}else if (d > f){
+    console.log("the first Number is largest")
+}else if (f > d){
+    console.log("the second Number is largest")
+}else if (f > g){
+    console.log("the second Number is largest")
+}else if (g > d){
+    console.log("the third Number is largest")
+}else if (g > f){
+    console.log("the third Number is largest")
+}else{
+    console.log("they are all the same")
+}
+
+//Q4
+let h = Number(prompt("how many sides does the triangle have in common"));
+if (h == 3){
+    console.log("you have a Equilateral")
+}else if (h == 2){
+    console.log("you have a Scalene")
+}else if (h == 0){
+    console.log("you have a Isosceles")
+}else{
+    console.log("retry")
+}
+
+//Q5
+let N1 = Number(prompt("what is your first number"));
+let N2 = Number(prompt("whats your second number"));
+let add = (N1 +=N2)
+let subract = (N1 -=N2)
+let multiply = (N1 *=N2)
+let divide = (N1 /=N2)
+let modulous = (N1 %=N2)
+let sum1 = Number(prompt("what is the mathematical operation"));
+if (sum1 = "add"){
+    console.log(add) 
+}else if (sum1 = "subract"){
+    console.log(subract)
 }
